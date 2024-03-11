69d988149c77e1da2c4869b001966f7fa9d9392d
Alex <14alex25@gmail.com>
2024-03-11 17:49:51 -0300

    feat: add get user endpoint

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -13,4 +13,14 @@ router.get('/', authMiddleware, async (req, res)=>{
     }
 })
 
+router.get('/:id', authMiddleware, async (req, res)=>{
+    try {
+        const [rowUser] = await db.execute(`SELECT u.*, '******' as senha FROM users u WHERE id = ? `, [id])
+        const user = rowUser && rowUser[0]
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
 module.exports = router;
