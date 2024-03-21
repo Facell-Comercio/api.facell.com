ec9ef4f71124ecbb1f5f673b0d4a768ce9f2f663
Alex <14alex25@gmail.com>
2024-03-21 16:19:58 -0300

    feat: adicionei novos métodos a financeiro/fornecedores

--- src/routes/financeiro/fornecedores/index.js
+++ src/routes/financeiro/fornecedores/index.js
@@ -2,29 +2,49 @@ const router = require('express').Router()
 
 const { getAll, toggleActive, update, insertOne } = require('../../../controllers/financeiro/fornecedores-controller');
 
-router.get('/', async (req, res)=>{
-    const result = await getAll(req)
-    res.status(200).json(result)
+router.get('/', async (req, res) => {
+    try {
+        const result = await getAll(req)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
-router.get('/', async (req, res)=>{
-    const result = await getAll(req)
-    res.status(200).json(result)
+router.get('/', async (req, res) => {
+    try {
+        const result = await getAll(req)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
-router.post('/', async (req, res)=>{
-    const result = await insertOne(req)
-    res.status(200).json(result)
+router.post('/', async (req, res) => {
+    try {
+        const result = await insertOne(req)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
-router.put('/', async (req, res)=>{
-    const result = await update(req)
-    res.status(200).json(result)
+router.put('/', async (req, res) => {
+    try {
+        const result = await update(req)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
-router.delete('/:id', async (req, res)=>{
-    const result = await toggleActive(req)
-    res.status(200).json(result)
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await toggleActive(req)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 module.exports = router;
