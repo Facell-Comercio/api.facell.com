23cbcf548b478a6f76edee36baa265713a73082d
jonathan <jonathanamarante@facell.com>
2024-04-05 18:13:17 -0300

    feat: getMyBudget de orçamentos

---
+++ src/controllers/financeiro/orcamento-controller.js
@@ -0,0 +1,334 @@
+const { db } = require("../../../mysql");
+
+function getAll(req) {
+  return new Promise(async (resolve, reject) => {
+    const { user } = req;
+    // user.perfil = 'Financeiro'
+    if (!user) {
+      reject("Usuário não autenticado!");
+      return false;
+    }
+    // Filtros
+    const { filters, pagination } = req.query;
+    const { pageIndex, pageSize } = pagination || {
+      pageIndex: 0,
+      pageSize: 15,
+    };
+    const { active, id_grupo_economico, nome, codigo } = filters || {};
+    // const { id_filial, termo } = filters || {id_filial: 1, termo: null}
+    console.log(filters);
+    var where = ` WHERE 1=1 `;
+    const params = [];
+
+    if (nome) {
+      where += ` AND fr.nome LIKE CONCAT(?,'%') `;
+      params.push(nome);
+    }
+    if (codigo) {
+      where += ` AND fr.codigo LIKE CONCAT(?,'%') `;
+      params.push(codigo);
+    }
+    if (id_grupo_economico) {
+      where += ` AND fr.id_grupo_economico = ? `;
+      params.push(id_grupo_economico);
+    }
+    if (active) {
+      where += ` AND fr.active = ? `;
+      params.push(active);
+    }
+
+    const offset = pageIndex * pageSize;
+
+    console.log(params);
+    try {
+      const [rowQtdeTotal] = await db.execute(
+        `SELECT 
+            COUNT(fr.id) as qtde  
+            FROM fin_rateio fr
+             ${where} `,
+        params
+      );
+      const qtdeTotal =
+        (rowQtdeTotal && rowQtdeTotal[0] && rowQtdeTotal[0]["qtde"]) || 0;
+
+      params.push(pageSize);
+      params.push(offset);
+      console.log(params);
+      var query = `
+            SELECT fr.id, fr.id_grupo_economico, fr.nome, fr.codigo, fr.active, ge.label as grupo_economico FROM fin_rateio fr
+            LEFT JOIN grupos_economicos ge ON fr.id_grupo_economico = ge.id
+            ${where}
+            ORDER BY fr.id DESC
+            LIMIT ? OFFSET ?
+            `;
+
+      // console.log(query)
+      // console.log(params)
+      const [rows] = await db.execute(query, params);
+
+      // console.log('Fetched Titulos', titulos.size)
+      // console.log(objResponse)
+      const objResponse = {
+        rows: rows,
+        pageCount: Math.ceil(qtdeTotal / pageSize),
+        rowCount: qtdeTotal,
+      };
+      resolve(objResponse);
+      // console.log(objResponse)
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
+
+function getMyBudget(req) {
+  return new Promise(async (resolve, reject) => {
+    // Filtros
+    const { filters, pagination } = req.query;
+    const { pageIndex, pageSize } = pagination || {
+      pageIndex: 0,
+      pageSize: 15,
+    };
+    const { termo, mes, ano } = filters || { mes: "4", ano: "2024" };
+    // const { id_filial, termo } = filters || {id_filial: 1, termo: null}
+    console.log(filters);
+    var where = ` WHERE 1=1 `;
+    const params = [];
+
+    if (termo) {
+      where += ` AND centro_custos LIKE CONCAT('%',?,'%') OR
+                    plano_contas LIKE CONCAT('%',?,'%') `;
+      params.push(termo);
+      params.push(termo);
+    }
+    if (mes && ano) {
+      where += ` AND fo.ref = ? `;
+      params.push(`${ano}-${("0" + `${mes}`).slice(-2)}-01`);
+    }
+    // if (id_grupo_economico) {
+    //   where += ` AND fr.id_grupo_economico = ? `;
+    //   params.push(id_grupo_economico);
+    // }
+    // if (active) {
+    //   where += ` AND fr.active = ? `;
+    //   params.push(active);
+    // }
+
+    const offset = pageIndex * pageSize;
+
+    console.log(params);
+    try {
+      const [rowQtdeTotal] = await db.execute(
+        `SELECT 
+            COUNT(foc.id) as qtde  
+            FROM fin_orcamento_contas foc
+            LEFT JOIN fin_centros_custo fcc ON fcc.id = foc.id_centro_custo
+            LEFT JOIN fin_plano_contas fpc ON fpc.id = foc.id_plano_contas
+            LEFT JOIN fin_orcamento as fo ON fo.id = foc.id_orcamento
+            LEFT JOIN grupos_economicos ge ON ge.id = fo.id_grupo_economico
+             ${where} `,
+        params
+      );
+      const qtdeTotal =
+        (rowQtdeTotal && rowQtdeTotal[0] && rowQtdeTotal[0]["qtde"]) || 0;
+
+      params.push(pageSize);
+      params.push(offset);
+      console.log(params);
+      var query = `
+            SELECT 
+              foc.*, 
+              fcc.nome as centro_custos, 
+              CONCAT(fpc.codigo," - ",fpc.descricao) as plano_contas, 
+              ge.label as grupo_economico, 
+              foc.valor_previsto - foc.saldo as realizado,
+              (foc.valor_previsto - foc.saldo) / foc.valor_previsto as realizado_percentual
+            FROM fin_orcamento_contas foc
+            LEFT JOIN fin_centros_custo fcc ON fcc.id = foc.id_centro_custo
+            LEFT JOIN fin_plano_contas fpc ON fpc.id = foc.id_plano_contas
+            LEFT JOIN fin_orcamento as fo ON fo.id = foc.id_orcamento
+            LEFT JOIN grupos_economicos ge ON ge.id = fo.id_grupo_economico
+            ${where}
+            LIMIT ? OFFSET ?
+            `;
+
+      // console.log(query)
+      // console.log(params)
+      const [rows] = await db.execute(query, params);
+
+      // console.log('Fetched Titulos', titulos.size)
+      // console.log(objResponse)
+      const objResponse = {
+        rows: rows,
+        pageCount: Math.ceil(qtdeTotal / pageSize),
+        rowCount: qtdeTotal,
+      };
+      resolve(objResponse);
+      // console.log(objResponse)
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
+
+function getOne(req) {
+  return new Promise(async (resolve, reject) => {
+    const { id } = req.params;
+    try {
+      const [rowRateios] = await db.execute(
+        `
+        SELECT 
+        fr.id, 
+        fr.id_grupo_economico, 
+        fr.nome, 
+        fr.codigo, 
+        fr.active,
+        fr.manual
+    FROM fin_rateio fr
+    WHERE fr.id = ?
+            `,
+        [id]
+      );
+      const rateios = rowRateios && rowRateios[0];
+      const [rowRateioItens] = await db.execute(
+        `
+            SELECT 
+              fri.id as id_item, 
+              fri.id_filial, 
+              FORMAT(fri.percentual * 100, 2) as percentual 
+            FROM fin_rateio_itens fri
+            LEFT JOIN fin_rateio fr ON fri.id_rateio = fr.id
+            WHERE fr.id = ?
+            `,
+        [id]
+      );
+      resolve({ ...rateios, itens: rowRateioItens });
+      return;
+    } catch (error) {
+      reject(error);
+      return;
+    }
+  });
+}
+
+function insertOne(req) {
+  return new Promise(async (resolve, reject) => {
+    const { active, id_grupo_economico, nome, codigo, manual, itens } =
+      req.body;
+    console.log(req.body);
+    const conn = await db.getConnection();
+    try {
+      if (!id_grupo_economico) {
+        throw new Error("ID_GRUPO_ECONOMICO não informado!");
+      }
+      if (!nome) {
+        throw new Error("NOME não informado!");
+      }
+      if (!codigo) {
+        throw new Error("CODIGO não informado!");
+      }
+      if (!manual && !itens?.length) {
+        throw new Error("ITENS não informados!");
+      }
+      if (manual === undefined) {
+        throw new Error("MANUAL não informado!");
+      }
+      await conn.beginTransaction();
+
+      // TODO Update do rateio
+      const [result] = await conn.execute(
+        `INSERT INTO fin_rateio (id_grupo_economico, nome, codigo, manual, active) VALUES (?,?,?,?,?)`,
+        [id_grupo_economico, nome, codigo, manual, active]
+      );
+
+      const newId = result.insertId;
+      if (!newId) {
+        throw new Error("Falha ao inserir o rateio!");
+      }
+
+      // TODO Inserir os itens
+      if (!manual) {
+        itens.forEach(async ({ id_filial, percentual }) => {
+          await conn.execute(
+            `INSERT INTO fin_rateio_itens (id_rateio, id_filial, percentual) VALUES(?,?,?)`,
+            [newId, id_filial, percentual]
+          );
+        });
+      }
+
+      await conn.commit();
+      resolve({ message: "Sucesso!" });
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+}
+
+function update(req) {
+  return new Promise(async (resolve, reject) => {
+    const { id, active, id_grupo_economico, nome, codigo, manual, itens } =
+      req.body;
+    console.log("REQ.BODY", req.body);
+    const conn = await db.getConnection();
+    try {
+      if (!id) {
+        throw new Error("ID não informado!");
+      }
+      if (!id_grupo_economico) {
+        throw new Error("ID_GRUPO_ECONOMICO não informado!");
+      }
+      if (!nome) {
+        throw new Error("NOME não informado!");
+      }
+      if (!codigo) {
+        throw new Error("CODIGO não informado!");
+      }
+      if (!manual && !itens?.length) {
+        throw new Error("ITENS não informados!");
+      }
+      if (manual === undefined) {
+        throw new Error("MANUAL não informado!");
+      }
+      await conn.beginTransaction();
+
+      // TODO Deletar os itens anteriores
+      await conn.execute(`DELETE FROM fin_rateio_itens WHERE id_rateio =?`, [
+        id,
+      ]);
+      // TODO Update do rateio
+      await conn.execute(
+        `UPDATE fin_rateio SET id_grupo_economico = ?, nome = ?, codigo = ?, manual =?, active = ? WHERE id =?`,
+        [id_grupo_economico, nome, codigo, manual, active, id]
+      );
+      // TODO Inserir os itens
+      if (!manual) {
+        itens.forEach(async ({ id_filial, percentual }) => {
+          await conn.execute(
+            `INSERT INTO fin_rateio_itens (id_rateio, id_filial, percentual) VALUES(?,?,?)`,
+            [id, id_filial, percentual]
+          );
+        });
+      }
+
+      console.log(itens);
+
+      await conn.commit();
+      resolve({ message: "Sucesso!" });
+    } catch (error) {
+      console.log(error);
+      conn.rollback();
+      reject(error);
+    }
+  });
+}
+
+module.exports = {
+  getAll,
+  getOne,
+  insertOne,
+  getMyBudget,
+  update,
+};
---
+++ src/routes/financeiro/orcamento/index.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+
+const {
+  getAll,
+  getOne,
+  update,
+  insertOne,
+  getMyBudget,
+} = require("../../../controllers/financeiro/orcamento-controller");
+const checkUserAuthorization = require("../../../middlewares/authorization-middleware");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await getAll(req);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/my-budget", async (req, res) => {
+  try {
+    const result = await getMyBudget(req);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const result = await getOne(req);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post(
+  "/",
+  checkUserAuthorization("FINANCEIRO", "OR", "MASTER"),
+  async (req, res) => {
+    try {
+      const result = await insertOne(req);
+      res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  }
+);
+
+router.put(
+  "/",
+  checkUserAuthorization("FINANCEIRO", "OR", "MASTER"),
+  async (req, res) => {
+    try {
+      const result = await update(req);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+);
+
+module.exports = router;
