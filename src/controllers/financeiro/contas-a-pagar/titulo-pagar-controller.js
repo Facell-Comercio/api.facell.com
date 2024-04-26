f9c081cf4329d18e0435bb598694bb707f2dc779
Alex <14alex25@gmail.com>
2024-04-25 22:39:40 -0300

    feat: finalizei update de titulo a pagar

--- src/controllers/financeiro/contas-a-pagar/titulo-pagar-controller.js
+++ src/controllers/financeiro/contas-a-pagar/titulo-pagar-controller.js
@@ -3,6 +3,7 @@ const { db } = require("../../../../mysql");
 const { checkUserDepartment } = require("../../../helpers/checkUserDepartment");
 const { checkUserPermission } = require("../../../helpers/checkUserPermission");
 const { normalizeFirstAndLastName, normalizeCurrency } = require("../../../helpers/mask");
+const { moverArquivoTempParaUploads } = require("../../files-controller");
 
 function getAll(req) {
   return new Promise(async (resolve, reject) => {
@@ -416,6 +417,8 @@ function update(req) {
     const conn = await db.getConnection();
 
     try {
+      const { user } = req;
+
       await conn.beginTransaction()
       const data = req.body
       const {
@@ -440,6 +443,7 @@ function update(req) {
 
         // Geral
         id_centro_custo,
+        centro_custo,
         data_emissao,
         data_vencimento,
         data_prevista,
@@ -452,11 +456,15 @@ function update(req) {
         id_tipo_solicitacao,
         descricao,
 
+        update_itens,
         itens,
+
+        update_rateio,
+        id_rateio,
         itens_rateio,
 
         url_nota_fiscal,
-        url_xml_nota,
+        url_xml,
         url_boleto,
         url_contrato,
         url_planilha,
@@ -535,107 +543,274 @@ function update(req) {
         throw new Error('Campo itens_rateio não informado!');
       }
 
-
-
       // Obter dados do Titulo no banco:
       const [rowTitulo] = await conn.execute(`SELECT * FROM fin_cp_titulos WHERE id = ?`, [id]);
       const titulo = rowTitulo && rowTitulo[0]
       if (!titulo) throw new Error('Título não localizado!');
-      console.log('TITULO_NO_BANCO', titulo)
 
-      // ^ Vamos validar se orçamento possui saldo:
-      // Passamos por cada item novo, analisando o orçamento
-      for (const item of itens) {
-        // ^ Validar item se possui todos os campos obrigatórios
-        if (!item.id_plano_conta) {
-          throw new Error(`O item não possui plano de contas selecionado! Item: ${JSON.stringify(item)}`)
+      // Obter os Itens anteriores para registra-los no histórico caso precise
+      const [itens_anteriores] = await conn.execute(`SELECT ti.valor, CONCAT(pc.codigo, ' - ', pc.descricao) as plano_conta
+        FROM fin_cp_titulos_itens ti
+        INNER JOIN fin_plano_contas pc ON pc.id = ti.id_plano_conta
+        WHERE ti.id_titulo = ?`, [titulo.id])
+
+      // Obter o Orçamento:
+      const [rowOrcamento] = await conn.execute(`SELECT id FROM fin_orcamento WHERE DATE_FORMAT(ref, '%Y-%m') = ? and id_grupo_economico = ?`, [format(titulo.created_at, 'yyyy-MM'), id_grupo_economico])
+
+      if (!rowOrcamento || rowOrcamento.length === 0) {
+        throw new Error('Orçamento não localizado!')
+      }
+      if (rowOrcamento.length > 1) {
+        throw new Error(`${rowOrcamento.length} orçamentos foram localizados, isso é um erro! Procurar a equipe de desenvolvimento.`)
+      }
+      const id_orcamento = rowOrcamento && rowOrcamento[0] && rowOrcamento[0]['id']
+
+      // ~ Início de Manipulação de Rateio //////////////////////
+      if (update_rateio) {
+        // ! Excluir Antigo Esquema de rateio
+        await conn.execute(`DELETE FROM fin_cp_titulos_rateio WHERE id_titulo = ?`, [id])
+
+        // Salvar Esquema de rateio
+        for (const item_rateio of itens_rateio) {
+          await conn.execute(`INSERT INTO fin_cp_titulos_rateio (id_titulo, id_rateio, id_filial, percentual) VALUES (?,?,?,?)`,
+            [id, id_rateio, item_rateio.id_filial, item_rateio.percentual])
         }
-        if (!item.valor) {
-          throw new Error(`O item não possui valor! Item: ${JSON.stringify(item)}`)
-        }
+      }
+      if (update_rateio || update_itens) {
+        // ! Excluir Antigos Itens Rateados
+        await conn.execute(`DELETE FROM fin_cp_titulos_rateio_itens WHERE id_titulo = ?`, [id])
+        // Vamos ratear e salvar os itens lá nos itens
+      }
+      // ~ Fim de Manipulação de Rateio //////////////////////
 
-        // Obter o registro de consumo anterior:
-        const [rowConsumo] = await conn.execute(`SELECT sum(foc.valor) as valor 
-        FROM fin_orcamento_consumo foc
-        WHERE foc.id_titulo_item IN (
-          SELECT ti.id
-          FROM fin_cp_titulos_itens ti
-          WHERE ti.id_titulo = ? AND ti.id_plano_conta = ?
-        )
-        GROUP BY foc.id
-      `, [id, item.id_plano_conta])
-        let valorConsumidoAnteriormente = rowConsumo && rowConsumo[0] || 0;
-        valorConsumidoAnteriormente = parseFloat(valorConsumidoAnteriormente);
-
-        // Obter o saldo do orçamento:
-        const [rowOrcamento] = await conn.execute(`SELECT id FROM fin_orcamento WHERE DATE_FORMAT(ref, '%y-%m') = ? and id_grupo_economico = ?`, [format(titulo.created_at, 'yyyy-MM'), id_grupo_economico])
-
-        if (!rowOrcamento || rowOrcamento.length === 0) {
+      //~ Início de manipulação de itens - caso update_itens = true //////////////////////
+      if (update_itens) {
+        if (!id_orcamento) {
           throw new Error('Orçamento não localizado!')
         }
-        if (rowOrcamento.length > 1) {
-          throw new Error(`${rowOrcamento.length} orçamentos foram localizados, isso é um erro! Procurar a equipe de desenvolvimento.`)
-        }
-        const id_orcamento = rowOrcamento && rowOrcamento[0] && rowOrcamento[0]['id']
 
-        // Obter a conta de orçamento:
-        const [rowContaOrcamento] = await conn.execute(`SELECT valor_previsto FROM fin_orcamento_contas WHERE id_orcamento = ? AND id_plano_contas = ? `, [id_orcamento, item.id_plano_conta])
+        // Passamos por cada item novo, validando campos e analisando o orçamento
+        for (const item of itens) {
+          // ^ Validar item se possui todos os campos obrigatórios
+          if (!item.id_plano_conta) {
+            throw new Error(`O item não possui plano de contas selecionado! Item: ${JSON.stringify(item)}`)
+          }
+          if (!item.valor) {
+            throw new Error(`O item não possui valor! Item: ${JSON.stringify(item)}`)
+          }
 
-        if (!rowContaOrcamento || rowContaOrcamento.length === 0) {
-          throw new Error('Não existe saldo no orçamento para o plano de contas!')
+          // ^ Vamos validar se orçamento possui saldo:
+          // Obter o registro de consumo anterior - para poder agregar no saldo atual do orçamento:
+          const [rowConsumoTitulo] = await conn.execute(`SELECT sum(foc.valor) as valor 
+            FROM fin_orcamento_consumo foc
+            WHERE foc.active = true AND foc.id_titulo_item IN (
+              SELECT ti.id
+              FROM fin_cp_titulos_itens ti
+              WHERE ti.id_titulo = ? AND ti.id_plano_conta = ?
+            )
+            GROUP BY foc.id
+          `, [id, item.id_plano_conta]);
+
+          let valorConsumidoPeloItemAnterior = rowConsumoTitulo && rowConsumoTitulo[0] && rowConsumoTitulo[0]['valor'] || 0;
+          valorConsumidoPeloItemAnterior = parseFloat(valorConsumidoPeloItemAnterior);
+
+          // Obter a Conta de Orçamento com o Valor Previsto:
+          const [rowOrcamentoConta] = await conn.execute(`SELECT id, valor_previsto FROM fin_orcamento_contas 
+          WHERE 
+            id_orcamento = ?
+            AND id_centro_custo = ?
+            AND id_plano_contas = ?
+            `, [id_orcamento, id_centro_custo, item.id_plano_conta])
+
+          if (!rowOrcamentoConta || rowOrcamentoConta.length === 0) {
+            throw new Error(`Não existe conta no orçamento para o seu Centro de custos + Plano de contas ${item.plano_conta}!`)
+          }
+          const id_orcamento_conta = rowOrcamentoConta && rowOrcamentoConta[0] && rowOrcamentoConta[0]['id']
+          let valor_previsto = rowOrcamentoConta && rowOrcamentoConta[0] && rowOrcamentoConta[0]['valor_previsto']
+          valor_previsto = parseFloat(valor_previsto);
+
+          // Obter o Valor Realizado da Conta do Orçamento:
+          const [rowConsumoOrcamento] = await conn.execute(`SELECT sum(valor) as valor 
+          FROM fin_orcamento_consumo 
+          WHERE active = true AND id_orcamento_conta = ?`, [id_orcamento_conta])
+          let valor_total_consumo = rowConsumoOrcamento && rowConsumoOrcamento[0] && rowConsumoOrcamento['valor'] || 0;
+          valor_total_consumo = parseFloat(valor_total_consumo);
+
+          // Calcular o saldo da conta do orçamento:
+          const saldo = valor_previsto - valor_total_consumo + valorConsumidoPeloItemAnterior;
+          if (saldo < item.valor) {
+            throw new Error(`Saldo insuficiente para o seu Centro de Custos + Plano de contas: ${item.plano_conta}. Necessário ${normalizeCurrency(item.valor - saldo)}`)
+          }
         }
-        let saldo = rowContaOrcamento && rowContaOrcamento[0] && rowContaOrcamento[0]['valor_previsto']
-        saldo = parseFloat(saldo) + valorConsumidoAnteriormente;
-        if (saldo < item.valor) {
-          throw new Error(`Saldo insuficiente para o plano de contas: ${item.plano_conta}. Necessário ${normalizeCurrency(item.valor - saldo)}`)
+
+        // ! Excluir todos os itens antigos do titulo
+        await conn.execute(`DELETE FROM fin_cp_titulos_itens WHERE id_titulo = ?`, [id])
+
+        // ! Excluir todas as contas de consumo do orçamento anteriores
+        await conn.execute(`DELETE FROM fin_orcamento_consumo 
+        WHERE id_titulo_item IN (
+          SELECT id FROM fin_cp_titulos_itens WHERE id_titulo = ?
+        )`, [id])
+
+        // * Salvar os novos itens
+        for (const item of itens) {
+          // * Persistir o item do titulo e obter o id:
+          const [result] = await conn.execute(`INSERT INTO fin_cp_titulos_itens (id_titulo, id_plano_conta, valor) VALUES (?,?,?)`, [id, item.id_plano_conta, item.valor])
+
+          // Obter o id_orcamento_conta
+          const [rowOrcamentoConta] = await conn.execute(`SELECT id, valor_previsto FROM fin_orcamento_contas 
+          WHERE 
+            id_orcamento = ?
+            AND id_centro_custo = ?
+            AND id_plano_contas = ?
+            `, [id_orcamento, id_centro_custo, item.id_plano_conta])
+
+          if (!rowOrcamentoConta || rowOrcamentoConta.length === 0) {
+            throw new Error(`Não existe conta no orçamento para o seu Centro de custos + Plano de contas ${item.plano_conta}!`)
+          }
+          const id_orcamento_conta = rowOrcamentoConta && rowOrcamentoConta[0] && rowOrcamentoConta[0]['id']
+
+          // * Persistir a conta de consumo do orçamento:
+          await conn.execute(`INSERT INTO fin_orcamento_consumo (id_orcamento_conta, id_titulo_item, valor) VALUES (?,?,?)`, [id_orcamento_conta, result.insertId, item.valor])
+        }
+      }
+      //~ Fim de manipulação de itens //////////////////////
+
+      if (update_itens || update_rateio) {
+        // Persistir os itens do rateio
+        const [itens_no_banco] = await conn.execute(`SELECT * FROM fin_cp_titulos_itens WHERE id_titulo = ?`, [id])
+        for (const item of itens_no_banco) {
+          // * Persistir o rateio dos itens
+          for (const item_rateio of itens_rateio) {
+            const valor_rateado = item_rateio.percentual * item.valor;
+            console.log(item_rateio.percentual, item.valor, valor_rateado)
+            await conn.execute(`INSERT INTO fin_cp_titulos_rateio_itens (id_titulo, id_titulo_item, id_rateio, id_filial, percentual, valor) VALUES (?,?,?,?,?,?)`,
+              [id, item.id, id_rateio, item_rateio.id_filial, item_rateio.percentual, valor_rateado])
+          }
         }
       }
 
+      // Persitir os anexos
+      const nova_url_nota_fiscal = await moverArquivoTempParaUploads(url_nota_fiscal);
+      const nova_url_xml = await moverArquivoTempParaUploads(url_xml);
+      const nova_url_boleto = await moverArquivoTempParaUploads(url_boleto);
+      const nova_url_contrato = await moverArquivoTempParaUploads(url_contrato);
+      const nova_url_planilha = await moverArquivoTempParaUploads(url_planilha);
+      const nova_url_txt = await moverArquivoTempParaUploads(url_txt);
 
-      throw new Error('Interrompi...')
-      // Itens anteriores
-      const [itens_anteriores] = await conn.execute(`SELECT ti.*, CONCAT(pc.codigo, ' - ', pc.descricao) as plano_conta
-      FROM fin_cp_titulos_itens t
-      INNER JOIN fin_plano_contas pc ON pc.id = ti.id_plano_conta
-      WHERE id_titulo = ?`, [titulo.id])
+      // Persistir  novos dados do Titulo
+      await conn.execute(`UPDATE fin_cp_titulos 
+      SET
+        id_fornecedor = ?,
+        id_banco = ?,
+        id_forma_pagamento = ?,
 
-      // todo validar campos obrigatórios, data, etc;
-      // todo: persistir titulo
-      // todo: persistir os itens do titulo
+        agencia = ?,
+        dv_agencia = ?,
+        id_tipo_conta = ?,
+        conta = ?,
+        dv_conta = ?,
+        favorecido = ?,
+        cnpj_favorecido = ?,
 
-      // todo: obter orçamento atual
+        id_tipo_chave_pix = ?,
+        chave_pix = ?,
 
-      // todo: obter os itens do titulo[]
-      // todo: persistir o esquema de rateio
-      // todo: persistir os titulo_rateio_itens
-      // todo: obter conta do orçamento
-      // todo: persistir o consumo do orçamento
+        id_tipo_solicitacao = ?,
+        id_filial = ?,
+        id_centro_custo = ?,
+        num_parcelas = ?,
+        parcela = ?,
+        
+        data_emissao = ?,
+        data_vencimento = ?,
+        data_prevista = ?,
+        num_doc = ?,
+        valor = ?,
+        descricao = ?,
+        
+        id_rateio = ?,
 
-      // todo: persitir os anexos
+        url_nota_fiscal = ?,
+        url_xml = ?,
+        url_boleto = ?,
+        url_contrato = ?,
+        url_planilha = ?,
+        url_txt = ?,
 
-      // todo: registar historico:
-      let modelo =
-        `EDITADO POR: ALEX BEZERRA. 
-      VENCIMENTO: DE: 29/04/2024 PARA: 25/04/2024
-      VALOR: DE: 200,00 PARA: 300,00 
-      RATEIO: DE: XXX - MANUAL PARA: R05 - RATEIO TODAS AS FILIAIS 
-      DESCRICAO: 
-        DE: 'DDKJKDJDKADJAKDADAKDAJKSDJAKJ' 
-        PARA: 'DJKADKAJDSAKDJADKASJDKASDJASKDJ431001'
-      CENTRO DE CUSTO: 
-        DE: ADMINISTRATIVO 
-        PARA: DP
-      ITENS ANTERIORES: 
-        1
-          PLANO DE CONTAS: 09.08.01 - COMPRA DE MATERIAL DE ALGO
-          VALOR: 150,00 
-        2
-          PLANO DE CONTAS: 09.08.01 - TESTE
-          VALOR: 300,00
-        `
+        updated_at = current_timestamp()
+
+      WHERE id = ?
+      `, [
+        id_fornecedor,
+        id_banco,
+        id_forma_pagamento,
+
+        agencia,
+        dv_agencia,
+        id_tipo_conta,
+        conta,
+        dv_conta,
+        favorecido,
+        cnpj_favorecido,
+
+        id_tipo_chave_pix,
+        chave_pix,
+
+        id_tipo_solicitacao,
+        id_filial,
+        id_centro_custo,
+
+        num_parcelas,
+        parcela,
+
+        data_emissao,
+        data_vencimento,
+        data_prevista,
+        num_doc,
+        valor,
+        descricao,
+
+        id_rateio,
+
+        nova_url_nota_fiscal,
+        nova_url_xml,
+        nova_url_boleto,
+        nova_url_contrato,
+        nova_url_planilha,
+        nova_url_txt,
+
+        // ID do título ao final!
+        id
+      ])
 
 
+      // Gerar e Registar historico:
+      let historico =
+        `EDITADO POR: ${normalizeFirstAndLastName(user.nome)}.\n`
+      if (data_vencimento != titulo.data_vencimento) {
+        historico += `VENCIMENTO: DE: ${format(titulo.data_vencimento, 'dd/MM/yyyy')} PARA: ${format(data_vencimento, 'dd/MM/yyyy')}\n`
+      }
+      if (valor != titulo.valor) {
+        historico += `VALOR: DE: ${normalizeCurrency(titulo.valor)} PARA: ${normalizeCurrency(valor)}\n`
+      }
+      if (descricao != titulo.descricao) {
+        historico += `DESCRICAO:\n \t DE: '${titulo.descricao}'\n \tPARA: '${descricao}'\n`
+      }
+      if (id_centro_custo != titulo.id_centro_custo) {
+        historico += `CENTRO DE CUSTO:\n \tDE: '${titulo.centro_custo}'\n \tPARA: '${centro_custo}'\n`
+      }
 
+      if (update_itens) {
+        historico += `ITENS ANTERIORES:\n `
+        itens_anteriores.forEach((item_anterior, index) => {
+          historico += `\t ITEM ${index + 1}: \n`;
+          historico += `\t PLANO DE CONTAS: '${item_anterior.plano_conta}' \n`
+          historico += `\t VALOR: '${normalizeCurrency(item_anterior.valor)}' \n`
+        })
+      }
+
+      await conn.execute(`INSERT INTO fin_cp_titulos_historico (id_titulo, descricao) VALUES (?,?)`, [id, historico])
 
       await conn.commit()
       resolve()
@@ -643,6 +818,9 @@ function update(req) {
       console.log('ERROR_TITULO_PAGAR_UPDATE', error)
       await conn.rollback()
       reject(error)
+
+    } finally {
+      await conn.release();
     }
   })
 }
@@ -686,23 +864,23 @@ function changeStatusTitulo(req) {
       if (titulo.id_status == '2') {
         //* O título constava como Negado, então agora que o status será alterado, devemos Ativar os registros de consumo:
         await conn.execute(`UPDATE fin_orcamento_consumo SET active = true
-        WHERE id_titulo_item 
-        IN (
-            SELECT ti.id
+        WHERE id_titulo_item
+      IN(
+        SELECT ti.id
             FROM fin_cp_titulos_itens ti
             WHERE ti.id_titulo = ?
         )`, [id_titulo])
       }
 
       // * Update fin_cp_titulos
-      await conn.execute(`UPDATE fin_cp_titulos SET id_status = ? WHERE id = ?`, [id_novo_status, id_titulo])
+      await conn.execute(`UPDATE fin_cp_titulos SET id_status = ? WHERE id = ? `, [id_novo_status, id_titulo])
 
       // !: Caso Negado - Inativar Consumo Orçamento 
       if (id_novo_status == '2') {
         await conn.execute(`UPDATE fin_orcamento_consumo SET active = false
-        WHERE id_titulo_item 
-        IN (
-            SELECT ti.id
+        WHERE id_titulo_item
+      IN(
+        SELECT ti.id
             FROM fin_cp_titulos_itens ti
             WHERE ti.id_titulo = ?
         )`, [id_titulo])
@@ -710,11 +888,11 @@ function changeStatusTitulo(req) {
 
       // !: Caso Diferente de Aprovado e Pago - Remover de Borderô 
       if (id_novo_status != '3' && id_novo_status != '4') {
-        await conn.execute(`DELETE FROM fin_cp_titulos_borderos WHERE id_titulo = ?`, [id_titulo])
+        await conn.execute(`DELETE FROM fin_cp_titulos_borderos WHERE id_titulo = ? `, [id_titulo])
       }
       let historico = ``
       let author = normalizeFirstAndLastName(user?.nome);
-      let textoMotivo = motivo ? ` MOTIVO: ${conn.escape(motivo)?.toUpperCase()}` : '';
+      let textoMotivo = motivo ? ` MOTIVO: ${conn.escape(motivo)?.toUpperCase()} ` : '';
 
       if (id_novo_status == '1') {
         historico = `RETORNADO PARA SOLICITADO POR: ${author}.`
@@ -725,12 +903,12 @@ function changeStatusTitulo(req) {
         historico += textoMotivo;
       }
       if (id_novo_status == '3') {
-        historico = `APROVADO POR: ${author}`
+        historico = `APROVADO POR: ${author} `
       }
 
       // ^ Gerar histórico no título
       if (historico) {
-        await conn.execute(`INSERT INTO fin_cp_titulos_historico (id_titulo, descricao) VALUES (?, ?)`, [id_titulo, historico])
+        await conn.execute(`INSERT INTO fin_cp_titulos_historico(id_titulo, descricao) VALUES(?, ?)`, [id_titulo, historico])
       }
 
       await conn.commit();
