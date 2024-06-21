584045bbecace95bd15546b254e6700653408593
jonathan <jonathanamarante@facell.com>
2024-06-21 16:37:55 -0300

    fix: deleteVencimento

--- src/controllers/financeiro/contas-a-pagar/borderos-controller.js
+++ src/controllers/financeiro/contas-a-pagar/borderos-controller.js
@@ -505,10 +505,7 @@ function deleteVencimento(req) {
         [id]
       );
 
-      if (
-        rowVencimento[0].id_status === 4 ||
-        !rowVencimento[0].data_pagamento
-      ) {
+      if (rowVencimento[0].id_status === 5 || rowVencimento[0].data_pagamento) {
         throw new Error("Não é possível remover do borderô vencimentos pagos!");
       }
 
@@ -628,7 +625,12 @@ function transferBordero(req) {
       await conn.commit();
       resolve({ message: "Sucesso!" });
     } catch (error) {
-      console.error("ERRO_TRANSFER_BORDERO", error);
+      logger.error({
+        module: "FINANCEIRO",
+        origin: "BORDEROS",
+        method: "TRANSFER_BORDERO",
+        data: { message: error.message, stack: error.stack, name: error.name },
+      });
       await conn.rollback();
       reject(error);
     } finally {
