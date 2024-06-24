d0b21876a184784a186be9609a344e8a65e3efa4
jonathan <jonathanamarante@facell.com>
2024-06-24 12:20:04 -0300

    feat: insertOne Bordero

--- src/controllers/financeiro/contas-a-pagar/borderos-controller.js
+++ src/controllers/financeiro/contas-a-pagar/borderos-controller.js
@@ -300,10 +300,99 @@ function insertOne(req) {
       // Inserir os vencimentos no borderô
 
       for (const vencimento of vencimentos) {
-        await conn.execute(
-          `INSERT INTO fin_cp_titulos_borderos (id_vencimento, id_bordero) VALUES(?,?)`,
-          [vencimento.id_vencimento, newId]
+        const [rowVencimento] = await conn.execute(
+          `SELECT id FROM fin_cp_titulos_borderos WHERE id_vencimento = ?`,
+          [vencimento.id_vencimento]
         );
+        if (rowVencimento.length === 0) {
+          await conn.execute(
+            `INSERT INTO fin_cp_titulos_borderos (id_vencimento, id_bordero) VALUES(?,?)`,
+            [vencimento.id_vencimento, newId]
+          );
+        }
+        // ^ Atualiza o vencimento com os dados da conciliação
+        const isParcial = vencimento.tipo_baixa === "PARCIAL";
+        const pago = !!vencimento.tipo_baixa;
+        await conn.execute(
+          `UPDATE fin_cp_titulos_vencimentos SET data_pagamento = ?, tipo_baixa = ?, valor_pago = ?, valor = ?, status = ?, obs = ? WHERE id = ?`,
+          [
+            pago ? new Date(data_pagamento) : null,
+            vencimento.tipo_baixa || null,
+            vencimento.valor_pago || null,
+            isParcial ? vencimento.valor_pago : vencimento.valor_total,
+            pago ? "pago" : "pendente",
+            pago ? "PAGAMENTO REALIZADO MANUALMENTE" : null,
+            vencimento.id_vencimento,
+          ]
+        );
+
+        //^ Se for com desconto ou acréscimo, devemos aplicar um ajuste nos itens rateados do título:
+        if (
+          vencimento.tipo_baixa === "COM DESCONTO" ||
+          vencimento.tipo_baixa === "COM ACRÉSCIMO"
+        ) {
+          const [itens_rateio] = await conn.execute(
+            `SELECT id FROM fin_cp_titulos_rateio WHERE id_titulo = ?`,
+            [vencimento.id_titulo]
+          );
+          // Aqui obtemos a diferença entre valor pago e valor do vencimento
+          const diferenca =
+            parseFloat(vencimento.valor_pago) -
+            parseFloat(vencimento.valor_total);
+          // Aqui geramos a diferença que será acrescida ou descontada de cada item rateio:
+          const difAplicada = diferenca / (itens_rateio?.length || 1);
+          // Aplicamos a diferença nos itens
+          await conn.execute(
+            "UPDATE fin_cp_titulos_rateio SET valor = valor + ? WHERE id_titulo = ?",
+            [difAplicada, vencimento.id_titulo]
+          );
+        }
+
+        if (vencimento.tipo_baixa === "PARCIAL") {
+          const valor =
+            parseFloat(vencimento.valor_total) -
+            parseFloat(vencimento.valor_pago);
+
+          // ^ Baixa parcial -> Cria um novo vencimento
+          await conn.execute(
+            `
+              INSERT INTO fin_cp_titulos_vencimentos (id_titulo, data_vencimento, data_prevista, valor, vencimento_origem) VALUES (?,?,?,?,?)
+            `,
+            [
+              vencimento.id_titulo,
+              new Date(vencimento.data_prevista_parcial),
+              new Date(vencimento.data_prevista_parcial),
+              valor.toFixed(2),
+              vencimento.id_vencimento,
+            ]
+          );
+        }
+        const [vencimentosNaoPagos] = await conn.execute(
+          `
+            SELECT 
+              tv.id, tb.id_bordero 
+            FROM fin_cp_titulos_vencimentos tv
+            LEFT JOIN fin_cp_titulos_borderos tb ON tb.id_vencimento = tv.id
+            WHERE tv.id_titulo = ? 
+            AND tv.data_pagamento IS NULL
+          `,
+          [vencimento.id_titulo]
+        );
+
+        if (pago && vencimentosNaoPagos.length === 0) {
+          // ^ Se todos os vencimentos estiverem pagos muda o status do titulo para pago
+          await conn.execute(
+            `UPDATE fin_cp_titulos SET id_status = ? WHERE id = ?`,
+            [5, vencimento.id_titulo]
+          );
+        }
+        if (pago && vencimentosNaoPagos.length > 0) {
+          // ^ Se houverem vencimentos ainda não pagos no título muda o status do titulo para pago parcial
+          await conn.execute(
+            `UPDATE fin_cp_titulos SET id_status = ? WHERE id = ?`,
+            [4, vencimento.id_titulo]
+          );
+        }
       }
 
       await conn.commit();
@@ -374,7 +463,6 @@ function update(req) {
 
       // Inserir os itens do bordero
       for (const vencimento of vencimentos) {
-        console.log(vencimento);
         const [rowVencimento] = await conn.execute(
           `SELECT id FROM fin_cp_titulos_borderos WHERE id_vencimento = ?`,
           [vencimento.id_vencimento]
@@ -385,18 +473,18 @@ function update(req) {
             [vencimento.id_vencimento, id]
           );
         }
-
         // ^ Atualiza o vencimento com os dados da conciliação
         const isParcial = vencimento.tipo_baixa === "PARCIAL";
+        const pago = !!vencimento.tipo_baixa;
         await conn.execute(
           `UPDATE fin_cp_titulos_vencimentos SET data_pagamento = ?, tipo_baixa = ?, valor_pago = ?, valor = ?, status = ?, obs = ? WHERE id = ?`,
           [
-            new Date(data_pagamento),
-            vencimento.tipo_baixa,
-            vencimento.valor_pago,
+            pago ? new Date(data_pagamento) : null,
+            vencimento.tipo_baixa || null,
+            vencimento.valor_pago || null,
             isParcial ? vencimento.valor_pago : vencimento.valor_total,
-            "pago",
-            "PAGAMENTO REALIZADO MANUALMENTE",
+            pago ? "pago" : "pendente",
+            pago ? "PAGAMENTO REALIZADO MANUALMENTE" : null,
             vencimento.id_vencimento,
           ]
         );
@@ -454,13 +542,14 @@ function update(req) {
           [vencimento.id_titulo]
         );
 
-        if (vencimentosNaoPagos.length === 0) {
+        if (pago && vencimentosNaoPagos.length === 0) {
           // ^ Se todos os vencimentos estiverem pagos muda o status do titulo para pago
           await conn.execute(
             `UPDATE fin_cp_titulos SET id_status = ? WHERE id = ?`,
             [5, vencimento.id_titulo]
           );
-        } else {
+        }
+        if (pago && vencimentosNaoPagos.length > 0) {
           // ^ Se houverem vencimentos ainda não pagos no título muda o status do titulo para pago parcial
           await conn.execute(
             `UPDATE fin_cp_titulos SET id_status = ? WHERE id = ?`,
@@ -1148,6 +1237,8 @@ function exportRemessa(req, res) {
               nome_sacado: borderoData.empresa_nome,
               num_inscricao_cedente: vencimento.favorecido_cnpj,
               nome_cedente: vencimento.favorecido_nome,
+              num_inscricao_sacador: vencimento.favorecido_cnpj,
+              nome_sacador: vencimento.favorecido_nome,
             });
             arquivo.push(segmentoJ);
             arquivo.push(segmentoJ52Pix);
