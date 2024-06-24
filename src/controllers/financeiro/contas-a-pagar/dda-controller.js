4373cfb52b41f154a601007ef1be00e825729af2
Alex <14alex25@gmail.com>
2024-06-24 12:21:19 -0300

    feat: ajustes em DDA

--- src/controllers/financeiro/contas-a-pagar/dda-controller.js
+++ src/controllers/financeiro/contas-a-pagar/dda-controller.js
@@ -2,6 +2,7 @@ const { db } = require("../../../../mysql");
 const { logger } = require("../../../../logger");
 const fs = require('fs/promises');
 const { remessaToObject } = require("../remessa/to-object");
+const { normalizeCodigoBarras } = require("../remessa/to-string/masks");
 
 async function getAll(req) {
     return new Promise(async (resolve, reject) => {
@@ -9,13 +10,15 @@ async function getAll(req) {
         const conn = await db.getConnection();
         try {
             const { filters, pagination } = req.query;
+            console.log(req.query)
             const { pageIndex, pageSize } = pagination || {
                 pageIndex: 0,
                 pageSize: 15,
             };
             const {
                 id_filial,
-                termo,
+                nome_fornecedor,
+                cod_barras,
                 vinculados,
                 naoVinculados,
                 tipo_data,
@@ -25,30 +28,39 @@ async function getAll(req) {
             let where = ` WHERE 1=1 `;
             const params = [];
 
+            if(vinculados !== undefined){
+                if(vinculados == 'false'){
+                    where += ` AND dda.id_vencimento IS NULL `;
+                }
+            }
             if (id_filial) {
                 where += ` AND f.id = ? `;
                 params.push(id_filial);
             }
 
-            if (termo) {
-                where += ` AND (
-                        dda.cod_barras LIKE CONCAT(?,"%") OR
-                        dda.nome_fornecedor LIKE CONCAT("%",?,"%") OR
-                      ) `;
-                params.push(termo);
-                params.push(termo);
+            if (nome_fornecedor) {
+                where += ` AND  dda.nome_fornecedor LIKE CONCAT("%",?,"%")  `;
+                params.push(nome_fornecedor);
             }
+
+            if (cod_barras) {
+                const codBarras = normalizeCodigoBarras(cod_barras)
+                where += ` AND dda.cod_barras = ?  `;
+                params.push(codBarras);
+            }
+
+
             if (tipo_data && range_data) {
                 const { from: data_de, to: data_ate } = range_data;
                 if (data_de && data_ate) {
-                    where += ` AND b.${tipo_data} BETWEEN '${data_de.split("T")[0]}' AND '${data_ate.split("T")[0]
+                    where += ` AND dda.${tipo_data} BETWEEN '${data_de.split("T")[0]}' AND '${data_ate.split("T")[0]
                         }'  `;
                 } else {
                     if (data_de) {
-                        where += ` AND b.${tipo_data} = '${data_de.split("T")[0]}' `;
+                        where += ` AND dda.${tipo_data} = '${data_de.split("T")[0]}' `;
                     }
                     if (data_ate) {
-                        where += ` AND b.${tipo_data} = '${data_ate.split("T")[0]}' `;
+                        where += ` AND dda.${tipo_data} = '${data_ate.split("T")[0]}' `;
                     }
                 }
             }
@@ -330,6 +342,12 @@ async function vincularDDA(req) {
                 throw new Error(`Vencimento de ID ${id_vencimento} não existe!`)
             }
 
+            // ^ Verificar se vencimento já foi vinculado
+            const [rowVencimentosVinculados] = await conn.execute(`SELECT id, cod_barras FROM fin_dda WHERE id_vencimento = ?`, [id_vencimento])
+            if(rowVencimentosVinculados && rowVencimentosVinculados.length > 0){
+                throw new Error(`Vencimento de ID ${id_vencimento} já foi vinculado com o DDA ID: ${rowVencimentosVinculados[0]['id']}, código de barras: ${rowVencimentosVinculados[0]['cod_barras']}!`)
+            }
+
             //^ Verificar se o registro no DDA já consta vinculado
             const [rowDDA] = await conn.execute(`SELECT id, id_vencimento FROM fin_dda 
              WHERE   
@@ -339,6 +357,7 @@ async function vincularDDA(req) {
                 throw new Error(`Registro ${id_dda} do DDA não existe!`)
             }
             const DDAbanco = rowDDA && rowDDA[0]
+            console.log({DDAbanco})
             //^ Se tem ID Vencimento no DDA então já está vinculado:
             if(DDAbanco.id_vencimento){
                 throw new Error(`Registro ${id_dda} do DDA já consta como vinculado, não deu para vincular com o id_vencimento ${DDAbanco.id_vencimento}`)
