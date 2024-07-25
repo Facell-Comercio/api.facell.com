5dc15848899bc52a064ea9cecf699d254e0e8e67
jonathan <jonathanamarante@facell.com>
2024-07-25 12:42:19 -0300

    fix: normalização do cod_barras no update da fatura

--- src/controllers/financeiro/contas-a-pagar/cartoes/updateFatura.js
+++ src/controllers/financeiro/contas-a-pagar/cartoes/updateFatura.js
@@ -1,6 +1,8 @@
 const { startOfDay } = require("date-fns");
 const { logger } = require("../../../../../logger");
 const { db } = require("../../../../../mysql");
+const { checkCodigoBarras } = require("../../../../helpers/chekers");
+const { normalizeCodigoBarras } = require("../../../../helpers/mask");
 
 module.exports = function updateFatura(req) {
   return new Promise(async (resolve, reject) => {
@@ -15,9 +17,14 @@ module.exports = function updateFatura(req) {
       if (!data_prevista) {
         throw new Error("Data de previsão de pagamento não informada!");
       }
+
+      const codBarras = normalizeCodigoBarras(cod_barras) || null;
+      if (!!codBarras && !checkCodigoBarras(codBarras)) {
+        throw new Error(`Código de barras inválido: ${cod_barras}`);
+      }
       await conn.execute(
         `UPDATE fin_cartoes_corporativos_faturas SET data_prevista = ?, cod_barras = ?, valor = ? WHERE id = ?`,
-        [startOfDay(data_prevista), cod_barras || null, valor, id]
+        [startOfDay(data_prevista), codBarras, valor, id]
       );
 
       resolve({ message: "Sucesso!" });
