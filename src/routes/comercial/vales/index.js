6ff25d457be43b30232297bf3f56f98656adb942
jonathan <jonathanamarante@facell.com>
2024-07-30 15:02:20 -0300

    fix: autorização getOne e getAll vales

--- src/routes/comercial/vales/index.js
+++ src/routes/comercial/vales/index.js
@@ -16,31 +16,23 @@ const {
 
 //! Refazer as validações de autorização
 
-router.get(
-  "/",
-  checkUserAuthorization("FINANCEIRO", "OR", "MASTER"),
-  async (req, res) => {
-    try {
-      const result = await getAll(req);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const result = await getAll(req);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-);
+});
 
-router.get(
-  "/:id",
-  checkUserAuthorization("FINANCEIRO", "OR", "MASTER"),
-  async (req, res) => {
-    try {
-      const result = await getOne(req);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const result = await getOne(req);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-);
+});
 
 router.get(
   "/abatimentos/:id",
