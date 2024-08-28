dc7ab3918427635d32b2a8856b36ba39a13e0171
jonathan <jonathanamarante@facell.com>
2024-08-28 14:57:31 -0300

    feat: filtragem do getAllRecorrencias

--- src/controllers/financeiro/contas-a-pagar/recorrencia/getAll.js
+++ src/controllers/financeiro/contas-a-pagar/recorrencia/getAll.js
@@ -1,48 +1,66 @@
 const { format } = require("date-fns");
 const { db } = require("../../../../../mysql");
-const { logger } = require("../../../../../logger");
-const { checkUserDepartment } = require("../../../../helpers/checkUserDepartment");
-const { checkUserPermission } = require("../../../../helpers/checkUserPermission");
+const {
+  logger,
+} = require("../../../../../logger");
+const {
+  checkUserDepartment,
+} = require("../../../../helpers/checkUserDepartment");
+const {
+  checkUserPermission,
+} = require("../../../../helpers/checkUserPermission");
 
-module.exports = function getAllRecorrencias(req) {
-    return new Promise(async (resolve, reject) => {
-
-      const { user } = req;
-      const conn = await db.getConnection();
-      const departamentosUser = user.departamentos.map(
-        (departamento) => departamento.id_departamento
+module.exports = function getAllRecorrencias(
+  req
+) {
+  return new Promise(async (resolve, reject) => {
+    const { user } = req;
+    const conn = await db.getConnection();
+    const departamentosUser =
+      user.departamentos.map(
+        (departamento) =>
+          departamento.id_departamento
       );
-  
-      try {
-        const { user } = req;
-        const { filters } = req.query || {};
-        const { mes, ano } = filters || {
-          mes: format(new Date(), "MM"),
-          ano: format(new Date(), "yyyy"),
-        };
-        const params = [];
-        let where = "WHERE 1=1 ";
-  
-        if (
-          !checkUserPermission(req, "MASTER") &&
-          !checkUserDepartment(req, "FINANCEIRO")
-        ) {
-          if (departamentosUser?.length > 0) {
-            where += ` AND (r.id_user = '${
-              user.id
-            }' OR t.id_departamento IN (${departamentosUser.join(",")})) `;
-          } else {
-            where += ` AND r.id_user = '${user.id}' `;
-          }
+
+    try {
+      const { user } = req;
+      const { filters } = req.query || {};
+      const { mes, ano, a_lancar } = filters || {
+        mes: format(new Date(), "MM"),
+        ano: format(new Date(), "yyyy"),
+      };
+
+      const params = [];
+      let where = "WHERE 1=1 ";
+
+      if (
+        !(
+          checkUserPermission(req, "MASTER") ||
+          checkUserDepartment(req, "FINANCEIRO")
+        )
+      ) {
+        if (departamentosUser?.length > 0) {
+          where += ` AND (r.id_user = '${
+            user.id
+          }' OR t.id_departamento IN (${departamentosUser.join(
+            ","
+          )})) `;
+        } else {
+          where += ` AND r.id_user = '${user.id}' `;
         }
-  
-        where += ` AND YEAR(r.data_vencimento) = ?
+      }
+
+      if (parseInt(a_lancar)) {
+        where += ` AND NOT r.lancado `;
+      }
+
+      where += ` AND YEAR(r.data_vencimento) = ?
           AND MONTH(r.data_vencimento) = ?`;
-        params.push(ano);
-        params.push(mes);
-  
-        const [recorrencias] = await conn.execute(
-          `SELECT 
+      params.push(ano);
+      params.push(mes);
+
+      const [recorrencias] = await conn.execute(
+        `SELECT 
             r.*,
             UPPER(t.descricao) as descricao, r.valor,
             forn.nome as fornecedor,
@@ -58,19 +76,23 @@ module.exports = function getAllRecorrencias(req) {
           ${where}
           ORDER BY r.data_vencimento
           `,
-          params
-        );
-        resolve({ rows: recorrencias });
-      } catch (error) {
-        logger.error({
-          module: "FINANCEIRO",
-          origin: "TITULOS A PAGAR",
-          method: "GET_ALL_RECORRENCIAS",
-          data: { message: error.message, stack: error.stack, name: error.name },
-        });
-        reject(error);
-      } finally {
-        conn.release();
-      }
-    });
-  }
+        params
+      );
+      resolve({ rows: recorrencias });
+    } catch (error) {
+      logger.error({
+        module: "FINANCEIRO",
+        origin: "TITULOS A PAGAR",
+        method: "GET_ALL_RECORRENCIAS",
+        data: {
+          message: error.message,
+          stack: error.stack,
+          name: error.name,
+        },
+      });
+      reject(error);
+    } finally {
+      conn.release();
+    }
+  });
+};
