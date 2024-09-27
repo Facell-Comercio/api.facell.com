f95236632af8aec452d16afaafe9d9ebfc528f3c
Jonathan <jonathan.almeida1793@gmail.com>
2024-09-27 12:46:28 -0300

    feat: imagem e url na notificação

--- src/controllers/notification-controller.js
+++ src/controllers/notification-controller.js
@@ -17,7 +17,7 @@ const isDevelopment = process.env.NODE_ENV == "development";
 //* [] Realiza o login no ngrok
 //* [] Usar o comando -> ngrok http 7000 (No terminal do ngrok)
 //* [] Substituir o valor da variável abaixo pelo "Fowarding"
-const httpsBaseURL = "https://b5e8-187-19-163-124.ngrok-free.app";
+const httpsBaseURL = "https://7630-187-19-163-124.ngrok-free.app";
 
 WebPush.setVapidDetails(isDevelopment ? httpsBaseURL : baseURL, publicKey, privateKey);
 
@@ -83,15 +83,17 @@ function registerUserMachine(req) {
 
 function sendNotificationUser(req) {
   return new Promise(async (resolve, reject) => {
-    const userId = req.params.id;
-    const { message, title } = req.body;
+    const { message, title, image_url, url, user_id, conn_externa } = req.body;
 
     let conn;
     try {
-      conn = await db.getConnection();
+      if (!message || !title) {
+        throw new Error("Dados insuficientes para a notificação!");
+      }
+      conn = conn_externa || (await db.getConnection());
       const [machines] = await conn.execute(
         "SELECT endpoint, public_key, auth FROM user_maquinas WHERE id_user = ?",
-        [userId]
+        [user_id]
       );
       if (!machines.length) {
         throw new Error("Usuário não encontrado");
@@ -111,19 +113,23 @@ function sendNotificationUser(req) {
           const payload = JSON.stringify({
             title,
             body: message,
+            image_url,
+            url,
           });
           await WebPush.sendNotification(subscription, payload);
         } catch (error) {
-          logger.error({
-            module: "ROOT",
-            origin: "NOTIFICATION",
-            method: "SEND_NOTIFICATION_USER",
-            data: {
-              message: `Erro ao enviar notificação para a maquina ${machine.endpoint}`,
-              stack: error.stack,
-              name: error.name,
-            },
-          });
+          if (error.name !== "WebPushError") {
+            logger.error({
+              module: "ROOT",
+              origin: "NOTIFICATION",
+              method: "SEND_NOTIFICATION_USER",
+              data: {
+                message: `Erro ao enviar notificação para a maquina ${machine.endpoint}`,
+                stack: error.stack,
+                name: error.name,
+              },
+            });
+          }
         }
       }
       resolve();
@@ -138,7 +144,7 @@ function sendNotificationUser(req) {
           name: error.name,
         },
       });
-      if (conn) await conn.rollback();
+
       reject(error);
     } finally {
       if (conn) conn.release();
@@ -148,45 +154,107 @@ function sendNotificationUser(req) {
 
 function sendNotificationUsers(req) {
   return new Promise(async (resolve, reject) => {
-    const { title, message } = req.body;
+    const { message, title, image_url, url, user_id_list, conn_externa } = req.body;
 
     let conn;
     try {
-      conn = await db.getConnection();
-      const [machines] = await conn.execute("SELECT endpoint, public_key, auth FROM user_maquinas");
-      if (!machines.length) {
-        throw new Error("Nenhum usuário encontrado");
+      if (!message || !title) {
+        throw new Error("Dados insuficientes para a notificação!");
       }
+      conn = conn_externa || (await db.getConnection());
 
-      //* Envia a notificação para todos os dispositivos registrados no banco de dados
-      for (const machine of machines) {
+      for (const user_id of user_id_list) {
         try {
-          const subscription = {
-            endpoint: machine.endpoint,
-            expirationTime: null,
-            keys: {
-              p256dh: machine.public_key,
-              auth: machine.auth,
+          await sendNotificationUser({
+            body: {
+              message,
+              title,
+              image_url,
+              url,
+              user_id,
+              conn_externa: conn,
             },
-          };
-          const payload = JSON.stringify({
-            title,
-            body: message,
           });
-          await WebPush.sendNotification(subscription, payload);
         } catch (error) {
-          logger.error({
-            module: "ROOT",
-            origin: "NOTIFICATION",
-            method: "SEND_NOTIFICATION_USER",
-            data: {
-              message: `Erro ao enviar notificação para a maquina ${machine.endpoint}`,
-              stack: error.stack,
-              name: error.name,
-            },
-          });
+          if (error.name !== "WebPushError") {
+            logger.error({
+              module: "ROOT",
+              origin: "NOTIFICATION",
+              method: "SEND_NOTIFICATION_USERS",
+              data: {
+                message: `Erro ao enviar notificação para o usuário de id ${user_id}`,
+                stack: error.stack,
+                name: error.name,
+              },
+            });
+          }
         }
       }
+
+      resolve();
+    } catch (error) {
+      logger.error({
+        module: "ROOT",
+        origin: "NOTIFICATION",
+        method: "SEND_NOTIFICATION_USERS",
+        data: {
+          message: error.message,
+          stack: error.stack,
+          name: error.name,
+        },
+      });
+      if (conn) await conn.rollback();
+      reject(error);
+    } finally {
+      if (conn) conn.release();
+    }
+  });
+}
+
+function sendNotificationAllUsers(req) {
+  return new Promise(async (resolve, reject) => {
+    const { message, title, image_url, url, conn_externa } = req.body;
+
+    let conn;
+    try {
+      if (!message || !title) {
+        throw new Error("Dados insuficientes para a notificação!");
+      }
+      conn = conn_externa || (await db.getConnection());
+      const [users] = await conn.execute(
+        `SELECT u.id, u.nome as nome_user FROM user_maquinas um
+        LEFT JOIN users u ON u.id = um.id_user
+        GROUP BY id_user`
+      );
+
+      for (const user of users) {
+        try {
+          await sendNotificationUser({
+            body: {
+              message,
+              title,
+              image_url,
+              url,
+              user_id: user.id,
+              conn_externa: conn,
+            },
+          });
+        } catch (error) {
+          if (error.name !== "WebPushError") {
+            logger.error({
+              module: "ROOT",
+              origin: "NOTIFICATION",
+              method: "SEND_NOTIFICATION_USERS",
+              data: {
+                message: `Erro ao enviar notificação para o usuário de id ${user.nome}`,
+                stack: error.stack,
+                name: error.name,
+              },
+            });
+          }
+        }
+      }
+
       resolve();
     } catch (error) {
       logger.error({
@@ -211,4 +279,5 @@ module.exports = {
   registerUserMachine,
   sendNotificationUser,
   sendNotificationUsers,
+  sendNotificationAllUsers,
 };
--- src/routes/notification-router.js
+++ src/routes/notification-router.js
@@ -3,6 +3,7 @@ const {
   registerUserMachine,
   sendNotificationUser,
   sendNotificationUsers,
+  sendNotificationAllUsers,
 } = require("../controllers/notification-controller");
 require("dotenv").config();
 
@@ -25,22 +26,31 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/send", async (req, res) => {
-  try {
-    const response = await sendNotificationUsers(req);
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+// router.post("/send", async (req, res) => {
+//   try {
+//     const response = await sendNotificationUser(req);
+//     res.status(200).json(response);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// });
 
-router.post("/send/:id", async (req, res) => {
-  try {
-    const response = await sendNotificationUser(req);
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+// router.post("/send/users", async (req, res) => {
+//   try {
+//     const response = await sendNotificationUsers(req);
+//     res.status(200).json(response);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// });
+
+// router.post("/send/all-users", async (req, res) => {
+//   try {
+//     const response = await sendNotificationAllUsers(req);
+//     res.status(200).json(response);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
