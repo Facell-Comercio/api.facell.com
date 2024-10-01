1cf6909766e571a975d252e4b5f7715e52cf3f7f
Jonathan <jonathan.almeida1793@gmail.com>
2024-10-01 17:06:52 -0300

    feat: CRUD tesouraria

--- src/controllers/financeiro/cadastros/contas-bancarias-controller.js
+++ src/controllers/financeiro/cadastros/contas-bancarias-controller.js
@@ -1,4 +1,4 @@
-const {logger} = require("../../../../logger");
+const { logger } = require("../../../../logger");
 const { db } = require("../../../../mysql");
 const { checkUserPermission } = require("../../../helpers/checkUserPermission");
 
@@ -32,15 +32,14 @@ function getAll(req) {
       active,
       id_matriz,
       onlyDatasys,
+      onlyCaixa,
     } = filters || {};
-    var where = ` WHERE 1=1 `;
+    let where = ` WHERE 1=1 `;
+
     const params = [];
 
     if (!isMaster) {
-      if (
-        !contas_bancarias_habilitadas ||
-        contas_bancarias_habilitadas.length === 0
-      ) {
+      if (!contas_bancarias_habilitadas || contas_bancarias_habilitadas.length === 0) {
         resolve({
           rows: [],
           pageCount: 0,
@@ -55,7 +54,7 @@ function getAll(req) {
       where += ` AND f.id = ? `;
       params.push(id_filial);
     }
-    if (id_matriz && id_matriz !== 'all') {
+    if (id_matriz && id_matriz !== "all") {
       where += ` AND f.id_matriz = ? `;
       params.push(id_matriz);
     }
@@ -68,7 +67,7 @@ function getAll(req) {
       else where += ` AND fb.nome LIKE CONCAT('%',?,'%') `;
       params.push(banco);
     }
-    if (id_grupo_economico && id_grupo_economico !== 'all') {
+    if (id_grupo_economico && id_grupo_economico !== "all") {
       where += ` AND ge.id = ? `;
       params.push(id_grupo_economico);
     }
@@ -79,6 +78,9 @@ function getAll(req) {
     if (active) {
       where += ` AND cb.active = ? `;
       params.push(active);
+    }
+    if (onlyCaixa && parseInt(onlyCaixa)) {
+      where += " AND cb.caixa";
     }
 
     const offset = pageIndex * pageSize;
@@ -95,14 +97,13 @@ function getAll(req) {
              ${where} `,
         params
       );
-      const qtdeTotal =
-        (rowQtdeTotal && rowQtdeTotal[0] && rowQtdeTotal[0]["qtde"]) || 0;
+      const qtdeTotal = (rowQtdeTotal && rowQtdeTotal[0] && rowQtdeTotal[0]["qtde"]) || 0;
 
       params.push(pageSize);
       params.push(offset);
       var query = `
             SELECT 
-              f.id_matriz, cb.id_filial, cb.id, cb.descricao, f.nome as filial, ge.nome, 
+              f.id_matriz, cb.*, f.nome as filial, ge.nome, 
               fb.nome as banco, ge.nome as grupo_economico, 
               ftc.tipo as tipo_conta, cb.active
             FROM fin_contas_bancarias cb
@@ -159,7 +160,6 @@ function getOne(req) {
       const planoContas = rowPlanoContas && rowPlanoContas[0];
       resolve(planoContas);
       return;
-      
     } catch (error) {
       logger.error({
         module: "FINANCEIRO",
@@ -197,11 +197,7 @@ function insertOne(req) {
         }
         campos += `${key}`;
         values += `?`;
-        params.push(
-          typeof rest[key] == "string"
-            ? rest[key].trim() || null
-            : rest[key] ?? null
-        ); // Adicionar valor do campo ao array de parâmetros
+        params.push(typeof rest[key] == "string" ? rest[key].trim() || null : rest[key] ?? null); // Adicionar valor do campo ao array de parâmetros
       });
 
       const query = `INSERT INTO fin_contas_bancarias (${campos}) VALUES (${values});`;
@@ -243,11 +239,7 @@ function update(req) {
           updateQuery += ", "; // Adicionar vírgula entre os campos
         }
         updateQuery += `${key} = ? `;
-        params.push(
-          typeof rest[key] == "string"
-            ? rest[key].trim() || null
-            : rest[key] ?? null
-        ); // Adicionar valor do campo ao array de parâmetros
+        params.push(typeof rest[key] == "string" ? rest[key].trim() || null : rest[key] ?? null); // Adicionar valor do campo ao array de parâmetros
       });
 
       params.push(id);
---
+++ src/controllers/financeiro/tesouraria/metodos/insertAdiantamento.js
@@ -0,0 +1,102 @@
+const { logger } = require("../../../../../logger");
+const { db } = require("../../../../../mysql");
+const crypto = require("crypto");
+const { formatDate } = require("../../../../services/boleto/helper/formatters");
+
+function objectToString(object) {
+  return Object.values(object).reduce((acc, value) => {
+    if (value instanceof Date) {
+      value = formatDate(value, "yyyyMMdd");
+    }
+    return acc + (value !== null && value !== undefined ? String(value) : "");
+  }, "");
+}
+module.exports = async (req) => {
+  return new Promise(async (resolve, reject) => {
+    const { user } = req;
+    const { id_conta_bancaria, valor, descricao } = req.body;
+
+    if (!user) {
+      reject("Usuário não autenticado!");
+      return false;
+    }
+
+    let conn;
+    try {
+      if (!id_conta_bancaria) {
+        throw new Error("ID da conta de saída não informado!");
+      }
+      if (!valor) {
+        throw new Error("Valor do adiantamento não informado!");
+      }
+      if (!descricao) {
+        throw new Error("Descrição do adiantamento não informada!");
+      }
+
+      conn = await db.getConnection();
+      await conn.beginTransaction();
+
+      const data_hoje = new Date();
+
+      const [rowContaBancaria] = await conn.execute(
+        "SELECT saldo, descricao FROM fin_contas_bancarias WHERE id = ?",
+        [id_conta_bancaria]
+      );
+      const contaBancaria = rowContaBancaria && rowContaBancaria[0];
+
+      if (parseFloat(contaBancaria.saldo) - parseFloat(valor) < 0) {
+        throw new Error("Adiantamento recusado, saldo insuficiente");
+      }
+
+      //* SAÍDA
+      await conn.execute(
+        "UPDATE fin_contas_bancarias SET saldo = saldo - ?, data_saldo = ? WHERE id = ?",
+        [valor, data_hoje, id_conta_bancaria]
+      );
+      const hashSaida = crypto
+        .createHash("md5")
+        .update(
+          objectToString({
+            id_conta_bancaria,
+            valor,
+            data_transferir: data_hoje,
+            id_user: user.id,
+            tipo_transacao: "DEBIT",
+            descricao,
+          })
+        )
+        .digest("hex");
+
+      await conn.execute(
+        `INSERT INTO fin_extratos_bancarios
+        (id_conta_bancaria, id_transacao, documento, data_transacao, tipo_transacao, valor, descricao, id_user, adiantamento)
+        VALUES(?,?,?,?,?,?,?,?,?)`,
+        [
+          id_conta_bancaria,
+          hashSaida,
+          hashSaida,
+          data_hoje,
+          "DEBIT",
+          -valor,
+          descricao.toUpperCase(),
+          user.id,
+          true,
+        ]
+      );
+
+      await conn.commit();
+      resolve({ message: "Sucesso" });
+    } catch (error) {
+      logger.error({
+        module: "FINANCEIRO",
+        origin: "TESOURARIA",
+        method: "INSERT_ADIANTAMENTO",
+        data: { message: error.message, stack: error.stack, name: error.name },
+      });
+      if (conn) await conn.rollback();
+      reject(error);
+    } finally {
+      conn.release();
+    }
+  });
+};
