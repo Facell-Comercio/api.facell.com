3958fe32ed024c56e3ea4b97b65d24e5b316909a
Alex Bezerra <14alex25@gmail.com>
2024-10-01 09:49:14 -0300

    feat: aplicação de recorrência reforço de conexão com o banco

--- mysql.js
+++ mysql.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql2/promise");
+const { logger } = require('./logger');
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV == 'production'
+const timeout = isProduction ? 1000 * 60 * 5 : 1000 * 60;
 const host = process.env.DB_HOST;
 const database = process.env.DB_DATABASE;
 const user = process.env.DB_USER;
@@ -16,10 +19,28 @@ const db = mysql.createPool({
   charset: "utf8mb4_general_ci",
   waitForConnections: true,
   connectTimeout: 60000,
-  connectionLimit: 150,
-  idleTimeout: 300000,
+  connectionLimit: 15,
 });
 
+async function mantainInfiniteConnection() {
+  let conn;
+  try {
+    conn = await db.getConnection();
+    await conn.execute('SELECT 1')
+
+    console.log('Conexão com o banco reforçada...');
+  } catch (error) {
+    logger.error({
+      module: 'ROOT',
+      origin: 'MYSQL',
+      data: { name: error.name, stack: error.stack, message: error.message }
+    })
+  } finally {
+    if (conn) conn.release();
+  }
+}
+setTimeout(mantainInfiniteConnection, timeout)
+
 module.exports = {
-    db
+  db
 };
